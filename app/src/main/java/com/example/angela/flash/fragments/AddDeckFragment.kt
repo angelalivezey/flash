f1c1a795ca547f1c5bf5b458b5c79fafec87d172
Angela Livezey <angelalivezey96@gmail.com>
2022-06-08 16:52:13 -0400

    add deck UI and fragment added

---
+++ app/src/main/java/com/example/angela/flash/fragments/AddDeckFragment.kt
@@ -0,0 +1,52 @@
+package com.example.angela.flash.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.angela.flash.Deck
+import com.example.angela.flash.DeckAdapter
+import com.example.angela.flash.DecksSharedViewModel
+import com.example.angela.flash.R
+import com.example.angela.flash.databinding.AddDeckFragmentBinding
+
+class AddDeckFragment : Fragment(R.layout.add_deck_fragment){
+    private lateinit var binding: AddDeckFragmentBinding
+    private lateinit var decksSharedViewModel: DecksSharedViewModel
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.add_deck_fragment,
+            container, false
+        )
+        decksSharedViewModel = ViewModelProvider(this).get(DecksSharedViewModel::class.java)
+        decksSharedViewModel = decksSharedViewModel
+        var adapter = DeckAdapter(decksSharedViewModel.deckList)
+        binding.buttonSaveNewDeck.setOnClickListener{
+            var deck = Deck()
+            deck.titleText = binding.enterDeckTitleBox.text.toString()
+            deck.color = binding.enterDeckColorBox.text.toString()
+            addNewDeck(deck)
+            adapter.notifyDataSetChanged()
+            findNavController().navigate(R.id.action_addDeckFragment_to_myDecksFragment)
+
+        }
+     return binding.root
+    }
+
+    fun addNewDeck(deck: Deck){
+        decksSharedViewModel.deckList.add(deck)
+    }
+}
